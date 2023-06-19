cba089a34491759649720214d775dffc403d8402
maekintosi <maciej@poppek.pl>
2023-06-19 16:03:46 +0200

    Changed nextauth path, finished authentication in the app, updated menu for logged in user

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,16 +1,27 @@
 'use client';
 
+import { User } from '@prisma/client';
+
+interface UserMenuProps {
+  currentUser?: User | null;
+}
+
 import { useState, useCallback } from 'react';
+import { signOut } from 'next-auth/react';
 
 import { AiOutlineMenu } from 'react-icons/ai';
 
 import useRegisterModal from '@/app/hooks/useRegisterModal';
+import useLoginModal from '@/app/hooks/useLoginModal';
 
 import Avatar from '../Avatar';
 import MenuItem from './MenuItem';
 
-const UserMenu = () => {
+const UserMenu: React.FC<UserMenuProps> = ({
+  currentUser,
+}) => {
   const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = useCallback(() => {
@@ -36,17 +47,51 @@ const UserMenu = () => {
       {isOpen && (
         <div className="absolute reounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
           <div className="flex flex-col cursor-pointer">
-            <MenuItem
-              onClick={() => {}}
-              label="Login"
-            />
-            <MenuItem
-              onClick={() => registerModal.onOpen}
-              label="Sign up"
-            />
+            {currentUser ? (
+              <>
+                <MenuItem
+                  onClick={() => false}
+                  label="My trips"
+                />
+                <MenuItem
+                  onClick={() => false}
+                  label="My favorites"
+                />
+                <MenuItem
+                  onClick={() => false}
+                  label="My reservations"
+                />
+                <MenuItem
+                  onClick={() => false}
+                  label="My properties"
+                />
+                <MenuItem
+                  onClick={() => false}
+                  label="Airbnb my home"
+                />
+                <hr />
+                <MenuItem
+                  onClick={() => signOut()}
+                  label="Logout"
+                />
+              </>
+            ) : (
+              <>
+                <MenuItem
+                  onClick={loginModal.onOpen}
+                  label="Login"
+                />
+                <MenuItem
+                  onClick={registerModal.onOpen}
+                  label="Sign up"
+                />
+              </>
+            )}
           </div>
         </div>
       )}
     </div>
   );
 };
+
+export default UserMenu;
