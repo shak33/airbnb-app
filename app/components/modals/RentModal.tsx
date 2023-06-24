0ad7bc6c109c2d483e5b18faa4a86290fc989c64
maekintosi <maciej@poppek.pl>
2023-06-24 23:39:08 +0200

    Created new component ImageUpload, added package next-cloudinary, connected app with cloudinary API, finished 4th step

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -9,6 +9,7 @@ import Heading from '../Heading';
 import CategoryInput from '@/app/components/inputs/CategoryInput';
 import CountrySelect from '@/app/components/inputs/CountrySelect';
 import Counter from '@/app/components/inputs/Counter';
+import ImageUpload from '@/app/components/inputs/ImageUpload';
 
 import useRentModal from '@/app/hooks/useRentModal';
 import Modal from './Modal';
@@ -57,6 +58,7 @@ const RentModal = () => {
   const guestCount = watch('guestCount');
   const roomCount = watch('roomCount');
   const bathroomCount = watch('bathroomCount');
+  const imageSrc = watch('imageSrc');
 
   const Map = useMemo(() => dynamic(() => import('@/app/components/Map'), {
     ssr: false,
@@ -162,6 +164,21 @@ const RentModal = () => {
           subtitle="How many bathrooms do you have?"
           value={bathroomCount}
           onChange={(value) => setCustomValue('bathroomCount', value)}
+        />
+      </div>
+    )
+  }
+
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Add a photo of your place"
+          subtitle="Show guests what your place looks like!"
+        />
+        <ImageUpload
+          onChange={(value) => setCustomValue('imageSrc', value)}
+          value={imageSrc}
         />
       </div>
     )
