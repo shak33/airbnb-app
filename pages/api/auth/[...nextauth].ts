8c483d6a84b779c41d4abcb38d862a3361cb04d7
shak33 <maciej@poppek.pl>
2023-09-07 21:59:01 +0200

    Another fucking fix

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -8,7 +8,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import prisma from '@/app/libs/prismadb';
 
 export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma) as any,
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
